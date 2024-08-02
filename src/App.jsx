26494256c04d314ed0a066aaf169558f5e5dbb97
masterchief164 <rishugupta2001.rg@gmail.com>
2024-08-02 16:10:46 +0530

    chore: added skeleton for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Sidebar from "./Components/Sidebar.jsx";
+import HomePage from "./Pages/Home/HomePage.jsx";
+import AboutPage from "./Pages/About/AboutPage.jsx";
+import AssetsPage from "./Pages/Assets/AssetsPage.jsx";
 
 function App() {
 
@@ -9,9 +12,9 @@ function App() {
         <BrowserRouter>
             <Sidebar />
             <Routes>
-                <Route path="/" element={<h1>Home</h1>} />
-                <Route path="/about" element={<h1>About</h1>} />
-                <Route path="/assets" element={<h1>Assets</h1>} />
+                <Route path="/" element={<HomePage/>} />
+                <Route path="/assets" element={<AssetsPage/>} />
+                <Route path="/about" element={<AboutPage/>} />
             </Routes>
         </BrowserRouter>
     </>
