5c3c0532b4d8f472a35033b7b11cd5d172a59a42
Siddhesh Pisal <siddheshpisal76@gmail.com>
2024-08-07 11:59:29 +0530

    chore: fixed some minor bugs

--- backend/controllers/watchlist.py
+++ backend/controllers/watchlist.py
@@ -1,5 +1,4 @@
 from flask import request
-
 from config.config import app
 from services import watchlist_service
 
---
+++ backend/models/Asset.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass
+class Asset:
+    __asset_id: int
+    __symbol: str
+    __sector: str
+    __asset_type: str
+
+
+
+
